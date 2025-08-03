432395e02760562b1ff3996ef79a53c7f94b2d6d
NoahAldhous <naldhous@hotmail.co.uk>
2025-08-03 14:18:24 +0100

    all create table queries written

--- main.py
+++ main.py
@@ -8,31 +8,49 @@ CREATE_USERS_TABLE = (
     "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, user_name TEXT, email TEXT, user_password TEXT, account_created DATE);"
 )
 
-CREATE_STRONGHOLD_CONSTRUCTION_LEVELS_TABLE = ()
-
-CREATE_STRONGHOLD_SIZE_LEVELS_TABLE = ()
-
-CREATE_CLASS_STRONGHOLD_ACTIONS_TABLE = ()
-
-CREATE_CLASS_DEMESNE_EFFECTS_TABLE = ()
-
-CREATE_CLASS_FEATURE_IMPROVEMENTS_TABLE = ()
-
-CREATE_STRONGHOLD_TOUGHNESS_LEVELS_TABLE = ()
-
-CREATE_STRONGHOLD_CLASSES_TABLE = ()
-
-CREATE_STRONGHOLD_SUB-TYPES_TABLE = ()
-
-CREATE_STRONGHOLD_TYPES_TABLE = ()
-
-CREATE_STRONGHOLD_TYPE_FEATURES_TABLE = ()
-
-CREATE_STRONGHOLDS_TABLE = (
-    "CREATE TABLE IF NOT EXISTS strongholds (id SERIAL PRIMARY KEY, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE, stronghold_name TEXT, level_id INTEGER, FOREIGN KEY(stronghold_type_id) REFERENCES stronghold_types(id), FOREIGN KEY(stronghold_sub-type_id) REFERENCES stronghold_sub-types(id), FOREIGN KEY(stronghold_class_id) REFERENCES stronghold_class(id),  created_at TIMESTAMP, )"
+CREATE_STRONGHOLD_CONSTRUCTION_LEVELS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_construction_levels (id SERIAL PRIMARY KEY, stronghold_level INTEGER, FOREIGN KEY(stronghold_type_id) REFERENCES stronghold_types(id), cost_to_build INTEGER, time_to_build INTEGER, fortification_morale_bonus INTEGER);"
 )
 
+CREATE_STRONGHOLD_SIZE_LEVELS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_size_levels (id SERIAL PRIMARY KEY, stronghold_level INTEGER, FOREIGN KEY(stronghold_type_id) REFERENCES stronghold_types(id), stronghold_size INTEGER);"
+)
 
+CREATE_CLASS_STRONGHOLD_ACTIONS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS class_stronghold_actions (id SERIAL PRIMARY KEY, FOREIGN KEY(stronghold_class_id) REFERENCES stronghold_classes(id), action_description TEXT);"
+)
+
+CREATE_CLASS_DEMESNE_EFFECTS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS class_demesne_effects (id SERIAL PRIMARY KEY, FOREIGN KEY(stronghold_class_id) REFERENCES stronghold_classes(id), effect_description TEXT);"
+)
+
+CREATE_CLASS_FEATURE_IMPROVEMENTS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS class_feature_improvements (id SERIAL PRIMARY KEY, FOREIGN KEY(stronghold_class_id) REFERENCES stronghold_classes(id), improvement_name TEXT, improvement_description TEXT, improvement_uses INTEGER);"
+)
+
+CREATE_STRONGHOLD_TOUGHNESS_LEVELS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_toughness_levels (id SERIAL PRIMARY KEY, level INTEGER, FOREIGN KEY(stronghold_class_id) REFERENCES stronghold_classes(id), stronghold_toughness INTEGER);"
+)
+
+CREATE_STRONGHOLD_CLASSES_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_classes (id SERIAL PRIMARY KEY, class_name TEXT, class_stronghold_name TEXT);"
+)
+
+CREATE_STRONGHOLD_SUBTYPES_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_subtypes (id SERIAL PRIMARY KEY, subtype_name TEXT);"
+)
+
+CREATE_STRONGHOLD_TYPES_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_types (id SERIAL PRIMARY KEY, type_name TEXT);"
+)
+
+CREATE_STRONGHOLD_TYPE_FEATURES_TABLE = (
+    "CREATE TABLE IF NOT EXISTS stronghold_type_features (id SERIAL PRIMARY KEY, FOREIGN KEY(stronghold_type_id) REFERENCES stronghold_types(id), feature_name TEXT, feature_description TEXT);"
+)
+
+CREATE_STRONGHOLDS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS strongholds (id SERIAL PRIMARY KEY, FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE, stronghold_name TEXT, stronghold_level INTEGER, FOREIGN KEY(stronghold_type_id) REFERENCES stronghold_types(id), FOREIGN KEY(stronghold_subtype_id) REFERENCES stronghold_subtypes(id), FOREIGN KEY(stronghold_class_id) REFERENCES stronghold_classes(id),  created_at TIMESTAMP);"
+)
 
 INSERT_USER_RETURN_ID = "INSERT INTO users (user_name, email, user_password, account_created) VALUES (%s, %s, %s, %s) RETURNING id;"
 
@@ -47,6 +65,11 @@ app = Flask(__name__)
 #This CANNOT be called url, it takes the env from smartpantry for some reason.
 dburl = os.getenv("STRONGHOLD_DATABASE_URL")
 connection = psycopg2.connect(dburl)
+
+#root route
+@app.get("/")
+def default_route():
+    return "hello world!"
 
 #create new user endpoint
 @app.post("/users/")
