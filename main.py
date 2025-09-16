f27419481313e499dd5c6ceb6dc5ad906a77cd13
NoahAldhous <naldhous@hotmail.co.uk>
2025-09-16 21:44:18 +0100

    changed order of blueprint registration

--- main.py
+++ main.py
@@ -30,17 +30,15 @@ def default_route():
 # BLUEPRINT ROUTES
 # /strongholds
 stronghold_types_bp.register_blueprint(stronghold_type_features_bp, url_prefix="/features")
+stronghold_classes_bp.register_blueprint(class_stronghold_actions_bp, url_prefix="/actions")
+
 strongholds_bp.register_blueprint(stronghold_types_bp, url_prefix="/types")
 strongholds_bp.register_blueprint(stronghold_toughness_levels_bp, url_prefix="/toughness")
 strongholds_bp.register_blueprint(stronghold_construction_levels_bp, url_prefix="/construction")
 strongholds_bp.register_blueprint(stronghold_size_levels_bp, url_prefix="/size")
-app.register_blueprint(strongholds_bp, url_prefix="/strongholds")
-
-#classes
 strongholds_bp.register_blueprint(stronghold_classes_bp, url_prefix="/classes")
-stronghold_classes_bp.register_blueprint(class_stronghold_actions_bp, url_prefix="/actions")
 
-# /users
+app.register_blueprint(strongholds_bp, url_prefix="/strongholds")
 app.register_blueprint(users_bp, url_prefix="/users")
 
 if __name__ == "__main__":
