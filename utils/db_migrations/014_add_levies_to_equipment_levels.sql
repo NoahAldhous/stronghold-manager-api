c1da3e411568cb3bcf3d2245bc890770d8b9aeb1
NoahAldhous <naldhous@hotmail.co.uk>
2025-10-27 16:00:09 +0000

    added levies migrations

---
+++ utils/db_migrations/014_add_levies_to_equipment_levels.sql
@@ -0,0 +1,9 @@
+INSERT INTO unit_equipment_levels (
+    level_name,
+    power_bonus,
+    defense_bonus
+) VALUES (
+    'levies',
+    0,
+    0
+);
---
+++ utils/db_migrations/015_add_levies_to_experience_levels.sql
@@ -0,0 +1,11 @@
+INSERT INTO unit_experience_levels (
+    level_name,
+    attack_bonus,
+    toughness_bonus,
+    morale_bonus
+) VALUES (
+    'levies',
+    '0',
+    '0',
+    '0'
+);
