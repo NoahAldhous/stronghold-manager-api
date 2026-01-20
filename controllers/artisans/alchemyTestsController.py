88833e8dea5f4e28cc6ca48e15eaae3598eab1d9
NoahAldhous <naldhous@hotmail.co.uk>
2026-01-20 12:12:23 +0000

    controller functions written for alchemy tests

---
+++ controllers/artisans/alchemyTestsController.py
@@ -0,0 +1,20 @@
+from models.Artisans.Alchemy_tests import CREATE_ALCHEMY_TESTS_TABLE, POPULATE_ALCHEMY_TESTS_TABLE
+from utils.db import query, execute
+
+# CREATE ALCHEMY TESTS TABLE
+def create_alchemy_tests_table():
+    res = execute(CREATE_ALCHEMY_TESTS_TABLE)
+    
+    if res:
+        return {"message" : "table created"}, 200
+    else: 
+        return {"message" : "error, could not create table"}, 404
+
+# POPULARE ALCHEMY TESTS TABLE    
+def populate_alchemy_tests_table():
+    res = execute(POPULATE_ALCHEMY_TESTS_TABLE)
+    
+    if res:
+        return {"message" : "table populated"}, 200
+    else: 
+        return {"message" : "error, could not populate table"}, 404
