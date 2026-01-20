2fd285a97928fb29a021f7a250326d870d39b8de
NoahAldhous <naldhous@hotmail.co.uk>
2026-01-20 13:28:10 +0000

    registered artisan blueprints

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from routes.stronghold_type_stats_bp import stronghold_type_stats_bp
 from routes.stronghold_treasury_bp import stronghold_treasury_bp
 from routes.units.units_bp import units_bp
 from routes.stronghold_benefits.raising_units_bp import raising_units_bp
+from routes.artisans import alchemy_tests_bp, artisan_bonuses_bp, artisan_shops_bp, artisan_upgrade_costs_bp, stronghold_artisans_bp
 import atexit
 
 app = Flask(__name__)
@@ -27,8 +28,7 @@ atexit.register(close_pool)
 @app.teardown_appcontext
 def on_teardown(exception):
     print("Teardown with exception", exception)
-
-    
+  
 # Dev-only manual shutdown route- avoid in prod
 @app.route("/shutdown")
 def shutdown():
@@ -58,6 +58,12 @@ def default_route():
 
 
 # BLUEPRINT ROUTES
+# /artisans
+stronghold_artisans_bp.register_blueprint(alchemy_tests_bp, url_prefix="/alchemy_tests")
+stronghold_artisans_bp.register_blueprint(artisan_bonuses_bp, url_prefix="/bonuses")
+stronghold_artisans_bp.register_blueprint(artisan_shops_bp, url_prefix="/shops")
+stronghold_artisans_bp.register_blueprint(artisan_upgrade_costs_bp, url_prefix="/upgade_costs")
+
 # /strongholds
 stronghold_types_bp.register_blueprint(stronghold_type_stats_bp, url_prefix="/stats")
 stronghold_types_bp.register_blueprint(stronghold_type_features_bp, url_prefix="/features")
@@ -72,6 +78,7 @@ strongholds_bp.register_blueprint(stronghold_construction_levels_bp, url_prefix=
 strongholds_bp.register_blueprint(stronghold_size_levels_bp, url_prefix="/size")
 strongholds_bp.register_blueprint(stronghold_classes_bp, url_prefix="/classes")
 strongholds_bp.register_blueprint(raising_units_bp, url_prefix="/raising_units")
+strongholds_bp.register_blueprint(stronghold_artisans_bp, url_prefix="/artisans")
 
 app.register_blueprint(strongholds_bp, url_prefix="/strongholds")
 app.register_blueprint(users_bp, url_prefix="/users")
