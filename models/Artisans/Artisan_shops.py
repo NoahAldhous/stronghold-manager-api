23f5b5cb7d67256eb086e3176c6bde9b86b90ea8
NoahAldhous <naldhous@hotmail.co.uk>
2026-01-20 16:30:56 +0000

    changed tailors shop name from shop to atelier so all shop names are unique

--- models/Artisans/Artisan_shops.py
+++ models/Artisans/Artisan_shops.py
@@ -83,7 +83,7 @@ POPULATE_ARTISAN_SHOPS_TABLE = (
             ),
             (
                 'tailor',
-                'shop',
+                'atelier',
                 'With a tailor in your retinue, you can, as a reaction, replace the result of any Charisma check with a 12 (before adding bonuses). This can be done a number of times equal to your tailor''s shop level, after which you must take an extended rest to refresh this ability.',
                 true
             )
