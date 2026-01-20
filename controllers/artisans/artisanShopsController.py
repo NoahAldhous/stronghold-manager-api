ac6f0beb9437cdda116fdfae5df491ec8ef87a91
NoahAldhous <naldhous@hotmail.co.uk>
2026-01-20 12:20:05 +0000

    controller functions for artisan shops

---
+++ controllers/artisans/artisanShopsController.py
@@ -0,0 +1,20 @@
+from models.Artisans.Artisan_shops import CREATE_ARTISAN_SHOPS_TABLE, POPULATE_ARTISAN_SHOPS_TABLE
+from utils.db import execute
+
+# CREATE ARTISAN SHOPS TABLE
+def create_artisan_shops_table():
+    res = execute(CREATE_ARTISAN_SHOPS_TABLE)
+    
+    if res:
+        return { "message" : "table created" }, 200
+    else: 
+        return { "message" : "error, could not create table" }, 404
+    
+# POPULATE ARTISAN SHOPS TABLE
+def populate_artisan_shops_table():
+    res = execute(POPULATE_ARTISAN_SHOPS_TABLE)
+    
+    if res:
+        return { "message" : "table populated" }
+    else:
+        return { "message" : "error, could not populate table" }
